cffe880d7b814031dcd9376f61594a4ff31d039f
Amani Kiruga <amanik@Amanis-MacBook-Air.local>
2024-12-29 07:53:36 -0500

    Clean gh-pages branch for new deployment

--- assets/css/fonts/convert.py
+++
@@ -1,8 +0,0 @@
-from fontTools.ttLib import TTFont
-import os
-
-for file in os.listdir("."):
-    if file.endswith("otf"):
-        with TTFont(file) as font:
-            font.flavor = "woff"
-            font.save(file.split(".")[0] + ".woff")
